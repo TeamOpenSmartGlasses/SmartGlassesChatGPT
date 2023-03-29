925f8a9618839df57d510f2156359dce49d269d5
Alex Israelov <alex1115alex@gmail.com>
2023-03-28 20:43:02 -0600

    * add openai-java library * add chatgpt command

--- app/src/main/java/com/teamopensourcesmartglasses/chatgpt/ChatGptBackend.java
+++ app/src/main/java/com/teamopensourcesmartglasses/chatgpt/ChatGptBackend.java
@@ -1,5 +1,74 @@
 package com.teamopensourcesmartglasses.chatgpt;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ChatGptBackend {
-    // Look at translationBackend
+    final String TAG = "SmartGlassesChatGpt_ChatGptBackend";
+    private String token;
+    private OpenAiService service;
+    public ChatGptBackend(){
+        token = System.getenv("OPENAI_TOKEN");
+        token = "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";
+        service = new OpenAiService(token);
+    }
+
+
+
+    public void sendChat(String message){
+        // Just an example of how this would work (pulled mostly from openai-java library example)...:
+        // 1. Send message to ChatGPT
+        // 2. Wait for response
+        // 3. On response, post a "ChatGptResponseReceivedEvent" with the data
+        // 3a) Subscribe to that event in ChatGptService.java and do something with the data (ie: send reference card to smart glasses)
+
+        class DoGptStuff implements Runnable {
+            public void run(){
+                Log.d(TAG, "Doing gpt stuff");
+                final List<ChatMessage> messages = new ArrayList<>();
+                final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are a dog and will speak as such.");
+                messages.add(systemMessage);
+
+                ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                        .model("gpt-3.5-turbo")
+                        .messages(messages)
+                        .n(5)
+                        .build();
+
+                List<ChatMessage> responses = null;
+
+                try {
+                    Log.d(TAG, "trying chatgpt stuff");
+                    responses = service.createChatCompletion(chatCompletionRequest)
+                            .getChoices()
+                            .stream()
+                            .map(ChatCompletionChoice::getMessage)
+                            .collect(Collectors.toList());
+
+                    Log.d(TAG, responses.toString());
+
+                    //TODO: find best response, emit an event to the ChatGptService
+                } catch (Exception e){
+                    Log.d(TAG, e.getMessage());
+                }
+            }
+        }
+        new Thread(new DoGptStuff()).start();
+    }
 }
--- app/src/main/java/com/teamopensourcesmartglasses/chatgpt/ChatGptService.java
+++ app/src/main/java/com/teamopensourcesmartglasses/chatgpt/ChatGptService.java
@@ -1,9 +1,88 @@
 package com.teamopensourcesmartglasses.chatgpt;
 
+import android.util.Log;
+
+import com.teamopensmartglasses.sgmlib.DataStreamType;
 import com.teamopensmartglasses.sgmlib.SGMCommand;
 import com.teamopensmartglasses.sgmlib.SGMLib;
 import com.teamopensmartglasses.sgmlib.SmartGlassesAndroidService;
 
-public class ChatGptService {
-    // Look at TranslationService
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.UUID;
+
+public class ChatGptService extends SmartGlassesAndroidService {
+    public final String TAG = "SmartGlassesChatGpt_ChatGptService";
+    static final String appName = "SmartGlassesChatGpt";
+    public ChatGptBackend chatGptBackend;
+
+    //our instance of the SGM library
+    public SGMLib sgmLib;
+
+    public ChatGptService(){
+        super(MainActivity.class,
+                "chatgpt_app",
+                1011,
+                appName,
+                "ChatGPT for smart glasses", com.google.android.material.R.drawable.notify_panel_notification_icon_bg);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /* Handle SGMLib specific things */
+
+        //Create SGMLib instance with context: this
+        sgmLib = new SGMLib(this);
+
+        //Define command with a UUID
+        UUID commandUUID = UUID.fromString("c3b5bbfd-4416-4006-8b40-73486ac37aec");
+
+        //Define list of phrases to be used to trigger the command
+        //currently just "Chat" as I don't know how our ASR would interpret "CHAT GEE-PEE-TEE"
+        String[] triggerPhrases = new String[]{"Chat"};
+
+        //Create command object
+        SGMCommand command = new SGMCommand(appName, commandUUID, triggerPhrases, "ChatGPT for your smart glasses!");
+
+        //Register the command
+        sgmLib.registerCommand(command, this::chatGptCommandCallback);
+
+        //Subscribe to transcription stream
+        sgmLib.subscribe(DataStreamType.TRANSCRIPTION_ENGLISH_STREAM, this::processTranscriptionCallback);
+
+        Log.d(TAG, "CHATGPT SERVICE STARTED");
+
+        /* Handle SmartGlassesChatGPT specific things */
+
+        EventBus.getDefault().register(this);
+
+        chatGptBackend = new ChatGptBackend();
+
+        chatGptBackend.sendChat("blah");
+
+        Log.d(TAG, "SERVO START");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy called");
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    public void chatGptCommandCallback(String args, long commandTriggeredTime) {
+        Log.d(TAG,"ChatGPT command callback called");
+    }
+
+    public void processTranscriptionCallback(String transcript, long timestamp, boolean isFinal){
+        Log.d(TAG, "Received transcription from SGM");
+
+        // Just an example of what you might want to do here...
+        if(isFinal){
+            chatGptBackend.sendChat(transcript);
+        }
+    }
+
 }
