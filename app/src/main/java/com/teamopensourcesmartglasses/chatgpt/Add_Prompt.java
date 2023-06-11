85c80840b39bc5c23c809e059a5209fbacb40252
Nicolo Micheletti <nicolo.micheletti@student.manchester.ac.uk>
2023-06-11 18:56:33 +0100

    Add delete button and block user from adding empty prompt

--- app/src/main/java/com/teamopensourcesmartglasses/chatgpt/Add_Prompt.java
+++ app/src/main/java/com/teamopensourcesmartglasses/chatgpt/Add_Prompt.java
@@ -2,12 +2,17 @@ package com.teamopensourcesmartglasses.chatgpt;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +21,7 @@ public class Add_Prompt extends AppCompatActivity {
     private EditText createPromptEditText;
     private RadioGroup radioGroupPrompts;
     private ArrayList<String> prompts;
+    private LinearLayout promptLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,7 @@ public class Add_Prompt extends AppCompatActivity {
 
         createPromptEditText = findViewById(R.id.addPromptTextView_createPrompt);
         radioGroupPrompts = findViewById(R.id.radioGroupPrompts);
+        promptLayout = findViewById(R.id.promptLayout);
 
         prompts = loadPrompts();
 
@@ -33,12 +40,44 @@ public class Add_Prompt extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String prompt = createPromptEditText.getText().toString().trim();
+
+                if (prompt.isEmpty()) {
+                    // Prompt is empty, show an error message or take appropriate action
+                    Toast.makeText(Add_Prompt.this, "Please enter a prompt", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (prompts.size() >= 5) {
+                    // Prompt limit reached, show an error message or take appropriate action
+                    Toast.makeText(Add_Prompt.this, "You can add at most 5 prompts", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 prompts.add(prompt);
 
                 // Create a new RadioButton and add it to the RadioGroup
                 RadioButton radioButton = new RadioButton(Add_Prompt.this);
                 radioButton.setText(prompt);
                 radioGroupPrompts.addView(radioButton);
+
+                // Create a delete button for the prompt
+                ImageButton deleteButton = new ImageButton(Add_Prompt.this);
+                deleteButton.setImageResource(R.drawable.ic_delete); // Replace with your desired delete icon
+                deleteButton.setBackgroundColor(Color.TRANSPARENT);
+                deleteButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Remove the prompt and associated views
+                        prompts.remove(prompt);
+                        radioGroupPrompts.removeView(radioButton);
+                        promptLayout.removeView(deleteButton);
+                        savePrompts(prompts);
+                    }
+                });
+
+
+                // Add the delete button to the layout
+                promptLayout.addView(deleteButton);
 
                 // Make the RadioGroup visible
                 radioGroupPrompts.setVisibility(View.VISIBLE);
@@ -58,9 +97,27 @@ public class Add_Prompt extends AppCompatActivity {
     private void populateSavedPrompts() {
         if (!prompts.isEmpty()) {
             for (String prompt : prompts) {
+                // Create a new RadioButton for each prompt
                 RadioButton radioButton = new RadioButton(Add_Prompt.this);
                 radioButton.setText(prompt);
                 radioGroupPrompts.addView(radioButton);
+
+                // Create a delete button for the prompt
+                Button deleteButton = new Button(Add_Prompt.this);
+                deleteButton.setText("Delete");
+                deleteButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Remove the prompt and associated views
+                        prompts.remove(prompt);
+                        radioGroupPrompts.removeView(radioButton);
+                        promptLayout.removeView(deleteButton);
+                        savePrompts(prompts);
+                    }
+                });
+
+                // Add the delete button to the layout
+                promptLayout.addView(deleteButton);
             }
 
             // Make the RadioGroup visible
