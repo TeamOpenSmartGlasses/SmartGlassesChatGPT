dbbc36d2c25154743fedf52555c00b853be42a62
Nicolo Micheletti <nicolo.micheletti@student.manchester.ac.uk>
2023-06-13 17:06:44 +0100

    Add add_prompt dialogue and backend logic

--- app/src/main/java/com/teamopensourcesmartglasses/chatgpt/Add_Prompt.java
+++ app/src/main/java/com/teamopensourcesmartglasses/chatgpt/Add_Prompt.java
@@ -1,141 +1,106 @@
 package com.teamopensourcesmartglasses.chatgpt;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.SharedPreferences;
-import android.graphics.Color;
+import androidx.room.Room;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.teamopensourcesmartglasses.chatgpt.dao.PromptDao;
+import com.teamopensourcesmartglasses.chatgpt.database.PromptDatabase;
+import com.teamopensourcesmartglasses.chatgpt.entities.Prompt;
+
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class Add_Prompt extends AppCompatActivity {
-    private EditText createPromptEditText;
+
+    private PromptDao promptDao;
     private RadioGroup radioGroupPrompts;
-    private ArrayList<String> prompts;
-    private LinearLayout promptLayout;
+    private FloatingActionButton addPromptFab;
+    private EditText titleEditText;
+    private EditText descriptionEditText;
+    private Button closeButton;
+
+    private List<Prompt> prompts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_prompt);
 
-        createPromptEditText = findViewById(R.id.addPromptTextView_createPrompt);
+        // Initialize the views
         radioGroupPrompts = findViewById(R.id.radioGroupPrompts);
-        promptLayout = findViewById(R.id.promptLayout);
+        addPromptFab = findViewById(R.id.addPromptFab);
+        titleEditText = findViewById(R.id.titleEditText);
+        descriptionEditText = findViewById(R.id.descriptionEditText);
+        closeButton = findViewById(R.id.closeButton);
 
-        prompts = loadPrompts();
+        // Initialize the database and get the DAO
+        PromptDatabase db = Room.databaseBuilder(getApplicationContext(), PromptDatabase.class, "prompt-database").build();
+        promptDao = db.getPromptDao();
 
-        // Add click listener to the "Add Prompt" button
-        Button addPromptButton = findViewById(R.id.addPromptButton);
-        addPromptButton.setOnClickListener(new View.OnClickListener() {
+        // Set up the FAB click listener
+        addPromptFab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String prompt = createPromptEditText.getText().toString().trim();
-
-                if (prompt.isEmpty()) {
-                    // Prompt is empty, show an error message or take appropriate action
-                    Toast.makeText(Add_Prompt.this, "Please enter a prompt", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (prompts.size() >= 5) {
-                    // Prompt limit reached, show an error message or take appropriate action
-                    Toast.makeText(Add_Prompt.this, "You can add at most 5 prompts", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                prompts.add(prompt);
-
-                // Create a new RadioButton and add it to the RadioGroup
-                RadioButton radioButton = new RadioButton(Add_Prompt.this);
-                radioButton.setText(prompt);
-                radioGroupPrompts.addView(radioButton);
-
-                // Create a delete button for the prompt
-                ImageButton deleteButton = new ImageButton(Add_Prompt.this);
-                deleteButton.setImageResource(R.drawable.ic_delete); // Replace with your desired delete icon
-                deleteButton.setBackgroundColor(Color.TRANSPARENT);
-                deleteButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Remove the prompt and associated views
-                        prompts.remove(prompt);
-                        radioGroupPrompts.removeView(radioButton);
-                        promptLayout.removeView(deleteButton);
-                        savePrompts(prompts);
-                    }
-                });
-
-
-                // Add the delete button to the layout
-                promptLayout.addView(deleteButton);
-
-                // Make the RadioGroup visible
-                radioGroupPrompts.setVisibility(View.VISIBLE);
-
-                // Clear the EditText
-                createPromptEditText.setText("");
-
-                // Save the updated prompts list
-                savePrompts(prompts);
+            public void onClick(View view) {
+                titleEditText.setVisibility(View.VISIBLE);
+                descriptionEditText.setVisibility(View.VISIBLE);
+                closeButton.setVisibility(View.VISIBLE);
             }
         });
 
-        // Load and populate saved prompts if available
-        populateSavedPrompts();
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = titleEditText.getText().toString();
+                String description = descriptionEditText.getText().toString();
+
+                if (!title.isEmpty() && !description.isEmpty()) {
+                    addNewPrompt(title, description);
+                    Toast.makeText(Add_Prompt.this, "Prompt added successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Add_Prompt.this, "Please fill out both fields", Toast.LENGTH_SHORT).show();
+                }
+                titleEditText.setText("");
+                descriptionEditText.setText("");
+                titleEditText.setVisibility(View.GONE);
+                descriptionEditText.setVisibility(View.GONE);
+                closeButton.setVisibility(View.GONE);
+            }
+        });
     }
 
-    private void populateSavedPrompts() {
-        if (!prompts.isEmpty()) {
-            for (String prompt : prompts) {
-                // Create a new RadioButton for each prompt
-                RadioButton radioButton = new RadioButton(Add_Prompt.this);
-                radioButton.setText(prompt);
-                radioGroupPrompts.addView(radioButton);
+    private void addNewPrompt(String title, String description) {
+        // Create a new Prompt instance
+        final Prompt prompt = new Prompt(title, description, 0);
 
-                // Create a delete button for the prompt
-                Button deleteButton = new Button(Add_Prompt.this);
-                deleteButton.setText("Delete");
-                deleteButton.setOnClickListener(new View.OnClickListener() {
+        // Insert the prompt into the database in a background thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                promptDao.insertPrompt(prompt);
+
+                // Update UI on the main thread
+                runOnUiThread(new Runnable() {
                     @Override
-                    public void onClick(View v) {
-                        // Remove the prompt and associated views
-                        prompts.remove(prompt);
-                        radioGroupPrompts.removeView(radioButton);
-                        promptLayout.removeView(deleteButton);
-                        savePrompts(prompts);
+                    public void run() {
+                        // Create a new RadioButton and add it to the RadioGroup
+                        RadioButton radioButton = new RadioButton(Add_Prompt.this);
+                        radioButton.setText(title);
+                        radioGroupPrompts.addView(radioButton);
+
+                        // Add prompt to the prompts list
+                        prompts.add(prompt);
                     }
                 });
-
-                // Add the delete button to the layout
-                promptLayout.addView(deleteButton);
             }
-
-            // Make the RadioGroup visible
-            radioGroupPrompts.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private void savePrompts(ArrayList<String> prompts) {
-        SharedPreferences sharedPreferences = getSharedPreferences("PromptPreferences", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        Set<String> promptSet = new HashSet<>(prompts);
-        editor.putStringSet("prompts", promptSet);
-        editor.apply();
-    }
-
-    private ArrayList<String> loadPrompts() {
-        SharedPreferences sharedPreferences = getSharedPreferences("PromptPreferences", MODE_PRIVATE);
-        Set<String> promptSet = sharedPreferences.getStringSet("prompts", new HashSet<String>());
-        return new ArrayList<>(promptSet);
+        }).start();
     }
 }
