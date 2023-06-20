5f7ba8562d7baba690b6c8e2a7be3a78403e1bc5
Nicolo Micheletti <nicolo.micheletti@student.manchester.ac.uk>
2023-06-21 00:39:50 +0100

    Migrate ui and utils

--- app/src/main/java/com/teamopensourcesmartglasses/chatgpt/UI/ChatGptSetupFragment.kt
+++ app/src/main/java/com/teamopensourcesmartglasses/chatgpt/UI/ChatGptSetupFragment.kt
@@ -1,37 +1,28 @@
-package com.teamopensourcesmartglasses.chatgpt.UI;
+package com.teamopensourcesmartglasses.chatgpt.UI
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.teamopensourcesmartglasses.chatgpt.databinding.FragmentChatgptsetupBinding
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import com.teamopensourcesmartglasses.chatgpt.ChatGptBackend;
-import com.teamopensourcesmartglasses.chatgpt.databinding.FragmentChatgptsetupBinding;
-
-public class ChatGptSetupFragment extends Fragment {
-
-    private FragmentChatgptsetupBinding binding;
-
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        binding = FragmentChatgptsetupBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+class ChatGptSetupFragment : Fragment() {
+    private var binding: FragmentChatgptsetupBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChatgptsetupBinding.inflate(inflater, container, false)
+        return binding!!.root
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
-
 }
--- app/src/main/java/com/teamopensourcesmartglasses/chatgpt/utils/Message.kt
+++ app/src/main/java/com/teamopensourcesmartglasses/chatgpt/utils/Message.kt
@@ -1,40 +1,11 @@
-package com.teamopensourcesmartglasses.chatgpt.utils;
-import androidx.annotation.NonNull;
+package com.teamopensourcesmartglasses.chatgpt.utils
 
-import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessage
+import java.time.LocalDateTime
 
-import java.time.LocalDateTime;
+class Message(var tokenCount: Int, val timestamp: LocalDateTime, val chatMessage: ChatMessage) {
 
-class Message {
-    private int tokenCount;
-    private final LocalDateTime timestamp;
-    private final ChatMessage message;
-
-    public Message(int tokenCount, LocalDateTime timestamp, ChatMessage message) {
-        this.tokenCount = tokenCount;
-        this.timestamp = timestamp;
-        this.message = message;
-    }
-
-    public int getTokenCount() {
-        return tokenCount;
-    }
-
-    public void setTokenCount(int tokenCount) {
-        this.tokenCount = tokenCount;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public ChatMessage getChatMessage() {
-        return message;
-    }
-
-    @NonNull
-    @Override
-    public String toString() {
-        return tokenCount + " " + message.toString();
+    override fun toString(): String {
+        return "$tokenCount $chatMessage"
     }
 }
